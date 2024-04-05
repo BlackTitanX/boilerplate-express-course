9838ff21e913d5de0a5b823a30001fc18f4d2779
Israel Perez Masle <91383194+BlackTitanX@users.noreply.github.com>
2024-04-05 08:36:01 -0400

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -40,6 +40,11 @@ app.get("/name",(req,res)=>{
 
 })
 
+app.post("/name",(req,res)=>{
+  res.json({"name": req.body.first + " " + req.body.last});
+
+})
+
 app.get('/now', function(req, res, next) {
   req.time = new Date().toString();
   next();
